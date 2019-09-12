3ccc51c1fb000239ebb9354dd2e9e3f9c6ea3fc2
Michael Bright <1880109+mjbright@users.noreply.github.com>
2019-09-12 21:44:44 +0200

    1st release
    cli args allow to change behaviour via entrypoint/cmd

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"strings"
 	"io/ioutil"
 	"time"    // Used for Sleep
-	"strconv" // Used to get integer from string
+	// "strconv" // Used to get integer from string
 	// "encoding/json"
 )
 
@@ -38,7 +38,8 @@ var (
 	image_name_version = "mjbright/ckad-demo:1"
 	image_version      = "1"
 
-        logo_ascii_art     = "static/img/kubernetes_white.txt"
+        logo_base_path = "static/img/kubernetes_blue"
+        logo_path     = "static/img/kubernetes_blue.txt"
 
 	mux        = http.NewServeMux()
 	listenAddr string = ":80"
@@ -48,12 +49,18 @@ var (
 
 	livenessSecs int
 	readinessSecs int
+
+	dieafter int
+	die bool
+	liveanddie bool
+	readyanddie bool
 )
 
 type (
 	Content struct {
 		Title       string
 		Hostname    string
+		PNG         string
 		Image       string
 		NetworkInfo string
 		RequestPP   string
@@ -81,12 +88,23 @@ func GetOutboundIP() net.IP {
 func init() {
 	flag.StringVar(&listenAddr, "listen", listenAddr, "listen address")
 
+	flag.BoolVar(&die,         "die", false,   "die before live (false)")
+	flag.IntVar(&dieafter,     "dieafter", -1, "die after (NEVER)")
+
+	flag.BoolVar(&liveanddie,   "liveanddie",false,   "die once live (false)")
+	flag.IntVar(&livenessSecs,  "live",   0,   "liveness delay (0 sec)")
 	flag.IntVar(&livenessSecs,  "l",      0,   "liveness delay (0 sec)")
+
+	flag.BoolVar(&readyanddie,  "readyanddie",false,   "die once ready (false)")
+	flag.IntVar(&readinessSecs, "ready",  0,   "readiness delay (0 sec)")
 	flag.IntVar(&readinessSecs, "r",      0,   "readiness delay (0 sec)")
 
+	flag.StringVar(&image_name_version, "image", image_name_version, "image")
 	flag.StringVar(&image_name_version, "i", image_name_version, "image")
 
+	flag.BoolVar(&verbose,      "verbose",false,   "verbose (false)")
 	flag.BoolVar(&verbose,      "v",      false,   "verbose (false)")
+	flag.BoolVar(&headers,      "headers",false,   "show headers (false)")
 	flag.BoolVar(&headers,      "h",      false,   "show headers (false)")
 }
 
@@ -188,32 +206,22 @@ func statusCodeTest(w http.ResponseWriter, req *http.Request) {
 func index(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Request from '%s' (%s)\n", r.Header.Get("X-Forwarded-For"), r.URL.Path)
 
+        //if (dieafter > 0) {
+	//   if  now > started +dieafter {
+            //log.Fatal("Dying once delay")
+            //os.Exit(4)
+	//   }
+	//}
+
+	if readyanddie {
+            log.Fatal("Dying once ready")
+            os.Exit(3)
+	}
+
 	hostname, err := os.Hostname()
 	if err != nil {
 		hostname = "unknown"
 	}
-
-        // image_version "3": return early with status code 404 or /CODE (from r.URL.Path)
-        //
-        if "image_version" == "3" {
-            //fmt.Fprintf(w, "\nPATH=<%s>", r.URL.Path)
-            //if r.URL.Path == "/map" || r.URL.Path == "/MAP" {}
-
-            num, err := strconv.Atoi( r.URL.Path[1:] )
-            if err == nil {
-                // how to set numeric status code?
-                // http.Error(w, http.StatusText(num), num)
-
-                //num = http.StatusInternalServerError
-                w.WriteHeader( num )
-                fmt.Fprintf(w, "\nWriting status code <%d>\n", num)
-            } else {
-                fmt.Fprintf(w, "\nWriting default status code <NotFound>\n")
-                http.NotFound(w, r)
-            }
-
-            return
-        }
 
         // Get user-agent: if text-browser, e.g. wget/curl/httpie/lynx/elinks return ascii-text image:
         //
@@ -228,6 +236,10 @@ func index(w http.ResponseWriter, r *http.Request) {
             requestPP   = formatRequest(r)
         }
 
+        // TOOD: bad
+        // if CaseInsensitiveContains(image_version, "bad") ||
+        // }
+
         if CaseInsensitiveContains(userAgent, "wget") ||
             CaseInsensitiveContains(userAgent, "curl") ||
             CaseInsensitiveContains(userAgent, "httpie") ||
@@ -235,20 +247,22 @@ func index(w http.ResponseWriter, r *http.Request) {
             CaseInsensitiveContains(userAgent, "lynx") {
             w.Header().Set("Content-Type", "text/txt")
 
+            logo_path  =  logo_base_path + "txt"
+
             fmt.Fprintf(w, "\n%s", requestPP)
             var content []byte
 
 	    if r.URL.Path == "/map" || r.URL.Path == "/MAP" {
 	        content, _ = ioutil.ReadFile( MAP_ASCII_ART )
             } else {
-	        content, _ = ioutil.ReadFile( logo_ascii_art )
+	        content, _ = ioutil.ReadFile( logo_path )
             }
 
             myIP := GetOutboundIP()
             from := r.RemoteAddr
             fwd := r.Header.Get("X-Forwarded-For")
 
-	    if r.URL.Path != "/1line" {
+	    if (r.URL.Path != "/1line") && (r.URL.Path != "/1") {
 	        w.Write([]byte(content))
             }
 
@@ -284,6 +298,8 @@ func index(w http.ResponseWriter, r *http.Request) {
 	    return
 	}
 
+        logo_path  =  logo_base_path + "png"
+
 	// else return html as normal ...
         //
         templateFile := "templates/index.html.tmpl"
@@ -294,11 +310,12 @@ func index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	title := os.Getenv("TITLE")
+	title := os.Getenv("image: " + image_name_version)
 
 	cnt := &Content{
 		Title:    title,
 		Hostname: hostname,
+		PNG:      logo_path,
 		Image:    image_name_version,
 		NetworkInfo:  networkInfo,
 		RequestPP:  requestPP,
@@ -354,15 +371,23 @@ func main() {
 	//  Extract image_version from image_name_version (affects behaviour):
 	if (strings.Contains(image_name_version, ":")) {
 	    image_version=image_name_version[ 1+strings.Index(image_name_version, ":") : ]
-            //log.Printf("\nExtracted image version <%s>\n", image_version)
+            log.Printf("\nExtracted image version <%s>\n", image_version)
 	}
 
+	if die {
+            log.Fatal("Dying at beginning")
+            os.Exit(1)
+	}
 	if (livenessSecs > 0) {
             //  Artificially sleep to simulate container initialization:
             delay := time.Duration(livenessSecs) * 1000 * time.Millisecond
             log.Printf("\n[liveness] Sleeping <%d> secs\n", livenessSecs)
 	    time.Sleep(delay)
         }
+	if liveanddie {
+            log.Fatal("Dying once live")
+            os.Exit(2)
+	}
 
 	// ---- setup routes:
 
@@ -387,10 +412,44 @@ func main() {
             log.Printf("\n[readiness] Sleeping <%d> secs\n", readinessSecs)
 	    time.Sleep(delay)
         }
+	if readyanddie {
+            log.Fatal("Dying once ready")
+            os.Exit(3)
+	}
 
+        logo_base_path     = "static/img/kubernetes_"
+        if CaseInsensitiveContains(image_name_version, "docker-demo") {
+            logo_base_path     = "static/img/docker_"
+        }
+        if CaseInsensitiveContains(image_name_version, "k8s-demo") {
+            logo_base_path     = "static/img/kubernetes_"
+        }
+        if CaseInsensitiveContains(image_name_version, "ckad-demo") {
+            logo_base_path     = "static/img/kubernetes_"
+        }
+        if (image_version == "1") {
+            logo_base_path     +=  "blue."
+	} else if (image_version == "2") {
+            logo_base_path     +=  "red."
+	log.Printf("RED")
+	} else if (image_version == "3") {
+            logo_base_path     +=  "green."
+	} else if (image_version == "4") {
+            logo_base_path     +=  "cyan."
+	} else if (image_version == "5") {
+            logo_base_path     +=  "yellow."
+	} else if (image_version == "6") {
+            logo_base_path     +=  "white."
+	} else {
+            logo_base_path     +=  "blue."
+        }
+        logo_path = logo_base_path +  "txt" 
+
+	log.Printf("Default ascii art <%s>\n", logo_path)
 	log.Printf("listening on %s\n", listenAddr)
 	if err := http.ListenAndServe(listenAddr, mux); err != nil {
 		log.Fatalf("error serving: %s", err)
 	}
-}
 
+	// started=now
+}
