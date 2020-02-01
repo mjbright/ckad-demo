f61a6f8d151f7195df99f9b01881c5009af30002
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-02-01 12:02:36 +0100

    Added use of env vars
    Removed tabs

--- main.go
+++ main.go
@@ -33,45 +33,44 @@ const (
 )
 
 var (
-	// -- defaults: can be overridden by cli ------------
-	// -- defaults: to be overridden by env/cli/cm ------
-	message            = ""
+    // -- defaults: can be overridden by cli ------------
+    // -- defaults: to be overridden by env/cli/cm ------
+    message            = ""
 
-	IMAGE_NAME_VERSION = "__IMAGE_NAME_VERSION__"
-	IMAGE_VERSION      = "__IMAGE_VERSION__"
-	DATE_VERSION       = "__DATE_VERSION__"
+    IMAGE_NAME_VERSION = os.Getenv("IMAGE_NAME_VERSION")
+    IMAGE_VERSION      = os.Getenv("IMAGE_VERSION")
+    DATE_VERSION       = os.Getenv("DATE_VERSION")
+    logo_base_path = os.Getenv("PICTURE_PATH_BASE")
+    logo_path      = logo_base_path + ".txt"
 
-        logo_base_path = "__PICTURE_PATH_BASE__"
-        logo_path      = logo_base_path + ".txt"
+    mux        = http.NewServeMux()
+    listenAddr string = ":80"
 
-	mux        = http.NewServeMux()
-	listenAddr string = ":80"
+    verbose    bool
+    headers    bool
 
-	verbose    bool
-	headers    bool
+    livenessSecs int
+    readinessSecs int
 
-	livenessSecs int
-	readinessSecs int
+    dieafter int
+    die  bool
+    liveanddie bool
+    readyanddie bool
 
-	dieafter int
-	die  bool
-	liveanddie bool
-	readyanddie bool
-
-	version  bool
+    version  bool
 )
 
 type (
-	Content struct {
-		Title       string
-		Hostname    string
-		Hosttype    string
-		Message     string
-		PNG         string
-		UsingImage  string
-		NetworkInfo string
-		FormattedReq   string
-	}
+    Content struct {
+        Title       string
+        Hostname    string
+        Hosttype    string
+        Message     string
+        PNG         string
+        UsingImage  string
+        NetworkInfo string
+        FormattedReq   string
+    }
 )
 
 // Get preferred outbound ip of this machine
@@ -93,11 +92,11 @@ func GetOutboundIP() net.IP {
 // Read command-line arguments:
 //
 func init() {
-	/* We would normally parse command-line arguments like this
-	   but we cannot pass arguments to flag.parse()
-	   so we cannot use ENV vars as something to be parsed as cli args:
-	flag.StringVar(&listenAddr, "listen", listenAddr, "listen address")
-	*/
+    /* We would normally parse command-line arguments like this
+       but we cannot pass arguments to flag.parse()
+       so we cannot use ENV vars as something to be parsed as cli args:
+    flag.StringVar(&listenAddr, "listen", listenAddr, "listen address")
+    */
 }
 
 // -----------------------------------
@@ -106,7 +105,7 @@ func init() {
 // load template file and substitute variables
 //
 func loadTemplate(filename string) (*template.Template, error) {
-	return template.ParseFiles(filename)
+    return template.ParseFiles(filename)
 }
 
 // -----------------------------------
@@ -196,132 +195,132 @@ func statusCodeTest(w http.ResponseWriter, req *http.Request) {
 // Main index handler - handles different requests
 //
 func index(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Request from '%s' (%s)\n", r.Header.Get("X-Forwarded-For"), r.URL.Path)
+    log.Printf("Request from '%s' (%s)\n", r.Header.Get("X-Forwarded-For"), r.URL.Path)
 
-        //if (dieafter > 0) {
-	//   if  now > started +dieafter {
-            //log.Fatal("Dying once delay")
-            //os.Exit(4)
-	//   }
-	//}
+    //if (dieafter > 0) {
+    //    if now > started +dieafter {
+    //        log.Fatal("Dying once delay")
+    //        os.Exit(4)
+    //   }
+    //}
 
-	if readyanddie {
-            log.Fatal("Dying once ready")
-            os.Exit(3)
-	}
+    if readyanddie {
+        log.Fatal("Dying once ready")
+        os.Exit(3)
+    }
 
-	hostName, err := os.Hostname()
-	if err != nil {
-		hostName = "unknown"
-	}
+    hostName, err := os.Hostname()
+    if err != nil {
+        hostName = "unknown"
+    }
 
-        // Get user-agent: if text-browser, e.g. wget/curl/httpie/lynx/elinks return ascii-text image:
-        //
-        userAgent := r.Header.Get("User-Agent")
+    // Get user-agent: if text-browser, e.g. wget/curl/httpie/lynx/elinks return ascii-text image:
+    //
+    userAgent := r.Header.Get("User-Agent")
 
-	// Enable/disable headers:
-        if r.URL.Path == "/headers"    { headers = true }
-        if r.URL.Path == "/no-headers" { headers = false }
+    // Enable/disable headers:
+    if r.URL.Path == "/headers"    { headers = true }
+    if r.URL.Path == "/no-headers" { headers = false }
 
-        multilineOP := (r.URL.Path != "/1line") && (r.URL.Path != "/1l") && (r.URL.Path != "/1")
+    multilineOP := (r.URL.Path != "/1line") && (r.URL.Path != "/1l") && (r.URL.Path != "/1")
 
-        networkInfo := ""
-        formattedReq   := ""
-        if verbose && multilineOP {
-            networkInfo = getNetworkInfo()
+    networkInfo := ""
+    formattedReq   := ""
+    if verbose && multilineOP {
+        networkInfo = getNetworkInfo()
+    }
+    if headers {
+        formattedReq   = formatRequest(r) + "\n"
+    }
+
+    // TOOD: bad
+    // if CaseInsensitiveContains(IMAGE_VERSION, "bad") ||
+    // }
+
+    hostType := ""
+    imageInfo := ""
+    htmlPageTitle := ""
+    msg := ""
+    if message != "" { msg = "'" + message + "'" }
+
+    if IMAGE_NAME_VERSION == "" {
+        hostType = "host"
+        htmlPageTitle = msg
+    } else {
+        hostType = "container"
+        imageInfo = "[image " + IMAGE_NAME_VERSION + "]"
+        htmlPageTitle = msg + " " + imageInfo
+    }
+
+    if CaseInsensitiveContains(userAgent, "wget") ||
+       CaseInsensitiveContains(userAgent, "curl") ||
+       CaseInsensitiveContains(userAgent, "http") ||
+       CaseInsensitiveContains(userAgent, "links") ||
+       CaseInsensitiveContains(userAgent, "lynx") {
+        w.Header().Set("Content-Type", "text/txt")
+
+        logo_path  =  logo_base_path + ".txt"
+
+        fmt.Fprintf(w, "%s", formattedReq)
+        var content []byte
+
+	if r.URL.Path == "/map" || r.URL.Path == "/MAP" {
+	    content, _ = ioutil.ReadFile( MAP_ASCII_ART )
+        } else {
+	    content, _ = ioutil.ReadFile( logo_path )
         }
-        if headers {
-            formattedReq   = formatRequest(r) + "\n"
+
+        myIP := GetOutboundIP()
+        from := r.RemoteAddr
+        fwd := r.Header.Get("X-Forwarded-For")
+
+        d := " "
+	if multilineOP {
+	    w.Write([]byte(content))
+            d = "\n"
         }
 
-        // TOOD: bad
-        // if CaseInsensitiveContains(IMAGE_VERSION, "bad") ||
-        // }
+        if fwd != "" { fwd=" [" + fwd + "]" }
 
-	hostType := ""
-	imageInfo := ""
-	htmlPageTitle := ""
-	msg := ""
-        if message != "" { msg = "'" + message + "'" }
-
-	if IMAGE_NAME_VERSION == "" {
-            hostType = "host"
-	    htmlPageTitle = msg
-	} else {
-            hostType = "container"
-	    imageInfo = "[image " + IMAGE_NAME_VERSION + "]"
-	    htmlPageTitle = msg + " " + imageInfo
-	}
-
-        if CaseInsensitiveContains(userAgent, "wget") ||
-            CaseInsensitiveContains(userAgent, "curl") ||
-            CaseInsensitiveContains(userAgent, "http") ||
-            CaseInsensitiveContains(userAgent, "links") ||
-            CaseInsensitiveContains(userAgent, "lynx") {
-            w.Header().Set("Content-Type", "text/txt")
-
-            logo_path  =  logo_base_path + ".txt"
-
-            fmt.Fprintf(w, "%s", formattedReq)
-            var content []byte
-
-	    if r.URL.Path == "/map" || r.URL.Path == "/MAP" {
-	        content, _ = ioutil.ReadFile( MAP_ASCII_ART )
-            } else {
-	        content, _ = ioutil.ReadFile( logo_path )
-            }
-
-            myIP := GetOutboundIP()
-            from := r.RemoteAddr
-            fwd := r.Header.Get("X-Forwarded-For")
-
-            d := " "
-	    if multilineOP {
-	        w.Write([]byte(content))
-                d = "\n"
-            }
-
-            if fwd != "" { fwd=" [" + fwd + "]" }
-
-	    p1 := fmt.Sprintf("Served from %s %s%s%s<%s>%s%s" + "Request from %s%s%s" + "%s%s" + "%s",
+	p1 := fmt.Sprintf("Served from %s %s%s%s<%s>%s%s" + "Request from %s%s%s" + "%s%s" + "%s",
 	        hostType, colour_me_yellow, hostName, imageInfo, myIP, colour_me_normal, d,
 		from,fwd, d,
 		networkInfo, d,
 	        msg)
 
-            fmt.Fprintf(w, p1)
-            if !multilineOP {
-                fmt.Fprintf(w, "\n")
-            }
-
-	    return
-	}
-
-        logo_path  =  logo_base_path + ".png"
-
-	// else return html as normal ...
-        //
-        templateFile := "templates/index.html.tmpl"
-
-	template, err := loadTemplate(templateFile)
-	if err != nil {
-            log.Printf("error loading template from %s: %s\n", templateFile, err)
-            return
-	}
-
-	cnt := &Content{
-		Title:    htmlPageTitle,
-		Hosttype: hostType,
-		Hostname: hostName,
-		Message:  message,
-		PNG:      logo_path,
-		UsingImage: imageInfo,
-		NetworkInfo:  networkInfo,
-		FormattedReq:  formattedReq,
+        fmt.Fprintf(w, p1)
+        if !multilineOP {
+            fmt.Fprintf(w, "\n")
         }
 
+	return
+    }
+
+    logo_path  =  logo_base_path + ".png"
+
+    // else return html as normal ...
+        //
+    templateFile := "templates/index.html.tmpl"
+
+    template, err := loadTemplate(templateFile)
+    if err != nil {
+        log.Printf("error loading template from %s: %s\n", templateFile, err)
+        return
+    }
+
+    cnt := &Content{
+        Title:    htmlPageTitle,
+        Hosttype: hostType,
+        Hostname: hostName,
+        Message:  message,
+        PNG:      logo_path,
+        UsingImage: imageInfo,
+        NetworkInfo:  networkInfo,
+        FormattedReq:  formattedReq,
+    }
+
         // apply Context values to template
-	template.Execute(w, cnt)
+    template.Execute(w, cnt)
 }
 
 func getNetworkInfo() string {
@@ -355,13 +354,13 @@ func getNetworkInfo() string {
 // Ping handler - echos back remote address
 //
 func ping(w http.ResponseWriter, r *http.Request) {
-	resp := fmt.Sprintf("ping: hello %s\n", r.RemoteAddr)
-	w.Write([]byte(resp))
+    resp := fmt.Sprintf("ping: hello %s\n", r.RemoteAddr)
+    w.Write([]byte(resp))
 }
 
 func showVersion(w http.ResponseWriter, r *http.Request) {
-	resp := fmt.Sprintf("version: %s [%s]\n", DATE_VERSION, IMAGE_NAME_VERSION)
-	w.Write([]byte(resp))
+    resp := fmt.Sprintf("version: %s [%s]\n", DATE_VERSION, IMAGE_NAME_VERSION)
+    w.Write([]byte(resp))
 }
 
 // -----------------------------------
@@ -370,110 +369,110 @@ func showVersion(w http.ResponseWriter, r *http.Request) {
 //
 //
 func main() {
-	f := flag.NewFlagSet("flag", flag.ExitOnError)
+    f := flag.NewFlagSet("flag", flag.ExitOnError)
 
-	f.StringVar(&listenAddr, "listen", listenAddr, "listen address")
+    f.StringVar(&listenAddr, "listen", listenAddr, "listen address")
 
-	f.BoolVar(&die,         "die", false,   "die before live (false)")
-	f.IntVar(&dieafter,     "dieafter", -1, "die after (NEVER)")
+    f.BoolVar(&die,         "die", false,   "die before live (false)")
+    f.IntVar(&dieafter,     "dieafter", -1, "die after (NEVER)")
 
-	f.BoolVar(&liveanddie,   "liveanddie",false,   "die once live (false)")
-	f.IntVar(&livenessSecs,  "live",   0,   "liveness delay (0 sec)")
-	f.IntVar(&livenessSecs,  "l",      0,   "liveness delay (0 sec)")
+    f.BoolVar(&liveanddie,   "liveanddie",false,   "die once live (false)")
+    f.IntVar(&livenessSecs,  "live",   0,   "liveness delay (0 sec)")
+    f.IntVar(&livenessSecs,  "l",      0,   "liveness delay (0 sec)")
 
-	f.BoolVar(&readyanddie,  "readyanddie",false,   "die once ready (false)")
-	f.IntVar(&readinessSecs, "ready",  0,   "readiness delay (0 sec)")
-	f.IntVar(&readinessSecs, "r",      0,   "readiness delay (0 sec)")
+    f.BoolVar(&readyanddie,  "readyanddie",false,   "die once ready (false)")
+    f.IntVar(&readinessSecs, "ready",  0,   "readiness delay (0 sec)")
+    f.IntVar(&readinessSecs, "r",      0,   "readiness delay (0 sec)")
 
-	//f.StringVar(&IMAGE_NAME_VERSION, "image", IMAGE_NAME_VERSION, "image")
-	//f.StringVar(&IMAGE_NAME_VERSION, "i", IMAGE_NAME_VERSION, "image")
+    //f.StringVar(&IMAGE_NAME_VERSION, "image", IMAGE_NAME_VERSION, "image")
+    //f.StringVar(&IMAGE_NAME_VERSION, "i", IMAGE_NAME_VERSION, "image")
 
-	f.StringVar(&message,            "message", "", "message")
+    f.StringVar(&message,            "message", "", "message")
 
-	f.BoolVar(&version,      "version", false,  "Show version and exit")
+    f.BoolVar(&version,      "version", false,  "Show version and exit")
 
-	f.BoolVar(&verbose,      "verbose",false,   "verbose (false)")
-	f.BoolVar(&verbose,      "v",      false,   "verbose (false)")
-	f.BoolVar(&headers,      "headers",false,   "show headers (false)")
-	f.BoolVar(&headers,      "h",      false,   "show headers (false)")
+    f.BoolVar(&verbose,      "verbose",false,   "verbose (false)")
+    f.BoolVar(&verbose,      "v",      false,   "verbose (false)")
+    f.BoolVar(&headers,      "headers",false,   "show headers (false)")
+    f.BoolVar(&headers,      "h",      false,   "show headers (false)")
 
-        // visitor := func(a *flag.Flag) { fmt.Println(">", a.Name, "value=", a.Value); }
-        // fmt.Println("Visit()"); f.Visit(visitor) fmt.Println("VisitAll()")
-        // f.VisitAll(visitor); fmt.Println("VisitAll()")
+    // visitor := func(a *flag.Flag) { fmt.Println(">", a.Name, "value=", a.Value); }
+    // fmt.Println("Visit()"); f.Visit(visitor) fmt.Println("VisitAll()")
+    // f.VisitAll(visitor); fmt.Println("VisitAll()")
 
-        if os.Getenv("CLI_ARGS") != "" {
-            cli_args := os.Getenv("CLI_ARGS")
-            a_cli_args := strings.Split(cli_args, " ")
+    if os.Getenv("CLI_ARGS") != "" {
+        cli_args := os.Getenv("CLI_ARGS")
+        a_cli_args := strings.Split(cli_args, " ")
 
-            f.Parse(a_cli_args)
-        } else {
-            f.Parse(os.Args[1:])
+        f.Parse(a_cli_args)
+    } else {
+        f.Parse(os.Args[1:])
+    }
+    // fmt.Println("Visit() after Parse()"); f.Visit(visitor);
+    // fmt.Println("VisitAll() after Parse()") f.VisitAll(visitor)
+
+    if verbose || version {
+        log.Printf("%s Version: %s\n", os.Args[0], DATE_VERSION)
+
+        if version {
+            os.Exit(0)
         }
-        // fmt.Println("Visit() after Parse()"); f.Visit(visitor);
-        // fmt.Println("VisitAll() after Parse()") f.VisitAll(visitor)
+        log.Printf("%s\n", strings.Join(os.Args, " "))
+    }
 
-        if verbose || version {
-            log.Printf("%s Version: %s\n", os.Args[0], DATE_VERSION)
+    if die {
+        log.Fatal("Dying at beginning")
+        os.Exit(1)
+    }
+    if (livenessSecs > 0) {
+        //  Artificially sleep to simulate container initialization:
+        delay := time.Duration(livenessSecs) * 1000 * time.Millisecond
+        log.Printf("[liveness] Sleeping <%d> secs\n", livenessSecs)
+        time.Sleep(delay)
+    }
+    if liveanddie {
+        log.Fatal("Dying once live")
+        os.Exit(2)
+    }
 
-	    if version {
-                os.Exit(0)
-	    }
-            log.Printf("%s\n", strings.Join(os.Args, " "))
+    // ---- setup routes: -------------------------------------------------
+
+    // ---- act as static web server on /static/*
+    mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+    mux.HandleFunc("/", index)
+    mux.HandleFunc("/test", statusCodeTest)
+
+    mux.HandleFunc("/version", showVersion)
+
+    mux.HandleFunc("/echo", formatRequestHandler)
+    mux.HandleFunc("/ECHO", formatRequestHandler)
+
+    mux.HandleFunc("/ping", ping)
+    mux.HandleFunc("/PING", ping)
+
+    mux.HandleFunc("/MAP", index)
+    mux.HandleFunc("/map", index)
+
+    if (readinessSecs > 0) {
+        //  Artificially sleep to simulate application initialization:
+        delay := time.Duration(readinessSecs) * 1000 * time.Millisecond
+        log.Printf("[readiness] Sleeping <%d> secs\n", readinessSecs)
+        time.Sleep(delay)
         }
+    if readyanddie {
+        log.Fatal("Dying once ready")
+        os.Exit(3)
+    }
 
-        if die {
-            log.Fatal("Dying at beginning")
-            os.Exit(1)
-        }
-        if (livenessSecs > 0) {
-            //  Artificially sleep to simulate container initialization:
-            delay := time.Duration(livenessSecs) * 1000 * time.Millisecond
-            log.Printf("[liveness] Sleeping <%d> secs\n", livenessSecs)
-            time.Sleep(delay)
-        }
-        if liveanddie {
-            log.Fatal("Dying once live")
-            os.Exit(2)
-	}
+    if verbose {
+        log.Printf("Default ascii art <%s>\n", logo_path)
+    }
 
-	// ---- setup routes: -------------------------------------------------
+    log.Printf("Now listening on %s\n", listenAddr)
+    if err := http.ListenAndServe(listenAddr, mux); err != nil {
+        log.Fatalf("error serving: %s", err)
+    }
 
-	// ---- act as static web server on /static/*
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
-	mux.HandleFunc("/", index)
-	mux.HandleFunc("/test", statusCodeTest)
-
-	mux.HandleFunc("/version", showVersion)
-
-	mux.HandleFunc("/echo", formatRequestHandler)
-	mux.HandleFunc("/ECHO", formatRequestHandler)
-
-	mux.HandleFunc("/ping", ping)
-	mux.HandleFunc("/PING", ping)
-
-	mux.HandleFunc("/MAP", index)
-	mux.HandleFunc("/map", index)
-
-	if (readinessSecs > 0) {
-            //  Artificially sleep to simulate application initialization:
-            delay := time.Duration(readinessSecs) * 1000 * time.Millisecond
-            log.Printf("[readiness] Sleeping <%d> secs\n", readinessSecs)
-	    time.Sleep(delay)
-        }
-	if readyanddie {
-            log.Fatal("Dying once ready")
-            os.Exit(3)
-	}
-
-	if verbose {
-            log.Printf("Default ascii art <%s>\n", logo_path)
-        }
-
-	log.Printf("Now listening on %s\n", listenAddr)
-	if err := http.ListenAndServe(listenAddr, mux); err != nil {
-		log.Fatalf("error serving: %s", err)
-	}
-
-	// started=now
+    // started=now
 }
