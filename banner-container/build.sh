f705f0b0261880dc2c760c5e60e38e3c13d8ec12
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-11-05 06:28:09 +0100

    Now with hello1_yellow/quiz_blue/vote_green images for html

--- banner-container/build.sh
+++ banner-container/build.sh
@@ -52,9 +52,9 @@ build_image() {
 ################################################################################
 # Main:
 
-build_image "hello1.txt"  ""  mjbright/banner:hello1 "OK" "OK"
-build_image "quiz.txt"    ""  mjbright/banner:quiz   "OK" "OK"
-build_image "vote.txt"    ""  mjbright/banner:vote   "OK" "OK"
+build_image "hello1_yellow.txt"  "hello_yellow.jpg"  mjbright/banner:hello1 "OK" "OK"
+build_image "quiz_blue.txt"      "quiz_blue.png"     mjbright/banner:quiz   "OK" "OK"
+build_image "vote_green.txt"     "vote_green.png"    mjbright/banner:vote   "OK" "OK"
 
 die "OK"
 
